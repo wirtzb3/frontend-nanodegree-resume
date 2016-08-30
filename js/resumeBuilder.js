07ecd8a692b38f2fbfd4a9108c7c90991e325257
Brittany Wirtz <brittanyjhanson@gmail.com>
2016-08-30 12:56:54 -0700

    Complete sections work, education, projects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -33,36 +33,101 @@ $("#main").append(bio.picture);
 $("#main").append(bio.welcomeMessage + ' ');
 $("#main").append(bio.skills);
 
-var work = {};
-work.currentJob = "nanny";
-work.yearsWorked = 8;
-work.city = "Seattle";
-
-var education = {};
-education["name"] = "University of Washington";
-education["years"] = "2007-2011";
-education["city"] = "Seattle, WA, US";
-
 $("#main").append(work["currentJob"]);
 $("#main").append(education.name);
 
+var work = {
+  "jobs": [
+  {
+    "employer": "Perry Household",
+    "title": "Nanny",
+    "location": "Wedgwood, Seattle, WA",
+    "dates": "January 2016 - Current",
+    "description": "Wrangler of two kiddos"
+  },
+  {
+    "employer": "Moore Household",
+    "title": "Household Manager/Nanny",
+    "location": "Renton, WA",
+    "dates": "February 2011 - June 2016",
+    "description": "Childcare, cook, homework help, driver"
+  },
+  {
+    "employer": "Ilgenfritz/Lamson Household",
+    "title": "Nanny",
+    "location": "North Beach, Seattle, WA",
+    "dates": "January 2009 - August 2012",
+    "description": "Three-kiddo controller"
+  },
+  {
+    "employer": "Marysville Parks and Recreation",
+    "title": "Athletic Assistant",
+    "location": "Marysville, WA",
+    "dates": "April 2005 - March 2012",
+    "description": "Basketball/community events extraordinnaire"
+  },
+  {
+    "employer": "Marysville City Clerk Office",
+    "title": "Office Assistant Intern",
+    "location": "Marysville, WA",
+    "dates": "Summer 2008",
+    "description": "MUNIS worker/inventoried city record vault"
+  },
+  ]
+}
+
 var projects = {
-  projects:["Portfolio", "Animal Trading Cards"]
+  "projects": [
+  {
+    "title": "Brittany's Portfolio",
+    "dates": "August 20, 2016",
+    "description": "Portfolio of Projects",
+    "images": ["image1", "image2", "image3"]
+  },
+  {
+    "title": "Animal Trading Cards",
+    "dates": "July 20, 2016",
+    "description": "Design a card referencing our favorite animal, with facts about the animal",
+    "images": ["image4", "image5", "image6"]
+  },
+  {
+    "title": "Mockup to Article",
+    "dates": "July 15, 2016",
+    "description": "Blog article into a website",
+    "images": ["image7", "image8", "image9"]
+  }
+  ]
 }
 
 var education = {
-  "schools": [
-  {
-    "name": "Udacity",
-    "city": "Online",
-    "degree": "Web Development",
-    "major": "Front-End Web Developer"
-  },
+  "onlineCourses": [
+    {
+    "title": "Front-End Web Developer Nanodegree",
+    "school": "Udacity",
+    "dates": "June 2016-March 2017",
+    "url": "https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001"
+    },
+    {
+    "title": "Javascript",
+    "school": "codecademy",
+    "dates": "June 2015 - September 2015",
+    "url": "https://www.codecademy.com/learn/javascript"
+    },
+    {
+    "title": "HTML & CSS",
+    "school": "codecademy",
+    "dates": "January 2015 - March 2015",
+    "url": "https://www.codecademy.com/learn/web"
+    }
+  ],
+ "schools": [
   {
     "name": "University of Washington",
-    "city": "Seattle",
-    "degree": "BS",
-    "major": "Molecular, Cellular, and Developmental Biology"
+    "location": "Seattle",
+    "degree": "Bachelor of Science",
+    "dates": "2007-2011",
+    "url": "http://www.washington.edu/",
+    "majors": ["Molecular Biology", "Cellular Biology", "Developmental Biology"]
   }
   ]
 }
